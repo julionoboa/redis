4b6bffac945abf48f756bce3af1353741b387eb2
Julio Noboa <julionoboa26@gmail.com>
2024-08-10 16:15:31 -0400

    modifications

--- index.js
+++ index.js
@@ -6,38 +6,38 @@ const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const provincias = [
-    { nombre: 'Azua', region: 'Sur' },
-    { nombre: 'Baoruco', region: 'Sur' },
-    { nombre: 'Barahona', region: 'Sur' },
-    { nombre: 'Dajabón', region: 'Noroeste' },
+    { nombre: 'Azua', region: 'South' },
+    { nombre: 'Baoruco', region: 'South' },
+    { nombre: 'Barahona', region: 'South' },
+    { nombre: 'Dajabón', region: 'Northeast' },
     { nombre: 'Distrito Nacional', region: 'Ozama' },
-    { nombre: 'Duarte', region: 'Nordeste' },
-    { nombre: 'Elías Piña', region: 'Sur' },
-    { nombre: 'El Seibo', region: 'Este' },
-    { nombre: 'Espaillat', region: 'Norte' },
-    { nombre: 'Hato Mayor', region: 'Este' },
-    { nombre: 'Hermanas Mirabal', region: 'Nordeste' },
-    { nombre: 'Independencia', region: 'Sur' },
-    { nombre: 'La Altagracia', region: 'Este' },
-    { nombre: 'La Romana', region: 'Este' },
-    { nombre: 'La Vega', region: 'Norte' },
-    { nombre: 'María Trinidad Sánchez', region: 'Nordeste' },
-    { nombre: 'Monseñor Nouel', region: 'Norte' },
-    { nombre: 'Monte Cristi', region: 'Noroeste' },
+    { nombre: 'Duarte', region: 'Northeast' },
+    { nombre: 'Elías Piña', region: 'South' },
+    { nombre: 'El Seibo', region: 'East' },
+    { nombre: 'Espaillat', region: 'North' },
+    { nombre: 'Hato Mayor', region: 'East' },
+    { nombre: 'Hermanas Mirabal', region: 'Northeast' },
+    { nombre: 'Independencia', region: 'South' },
+    { nombre: 'La Altagracia', region: 'East' },
+    { nombre: 'La Romana', region: 'East' },
+    { nombre: 'La Vega', region: 'North' },
+    { nombre: 'María Trinidad Sánchez', region: 'NordEast' },
+    { nombre: 'Monseñor Nouel', region: 'North' },
+    { nombre: 'Monte Cristi', region: 'Northeast' },
     { nombre: 'Monte Plata', region: 'Ozama' },
-    { nombre: 'Pedernales', region: 'Sur' },
-    { nombre: 'Peravia', region: 'Sur' },
-    { nombre: 'Puerto Plata', region: 'Norte' },
-    { nombre: 'Samaná', region: 'Nordeste' },
+    { nombre: 'Pedernales', region: 'South' },
+    { nombre: 'Peravia', region: 'South' },
+    { nombre: 'Puerto Plata', region: 'North' },
+    { nombre: 'Samaná', region: 'NordEast' },
     { nombre: 'San Cristóbal', region: 'Ozama' },
-    { nombre: 'San José de Ocoa', region: 'Sur' },
-    { nombre: 'San Juan', region: 'Sur' },
-    { nombre: 'San Pedro de Macorís', region: 'Este' },
-    { nombre: 'Sánchez Ramírez', region: 'Norte' },
-    { nombre: 'Santiago', region: 'Norte' },
-    { nombre: 'Santiago Rodríguez', region: 'Noroeste' },
+    { nombre: 'San José de Ocoa', region: 'South' },
+    { nombre: 'San Juan', region: 'South' },
+    { nombre: 'San Pedro de Macorís', region: 'East' },
+    { nombre: 'Sánchez Ramírez', region: 'North' },
+    { nombre: 'Santiago', region: 'North' },
+    { nombre: 'Santiago Rodríguez', region: 'Northeast' },
     { nombre: 'Santo Domingo', region: 'Ozama' },
-    { nombre: 'Valverde', region: 'Noroeste' }
+    { nombre: 'Valverde', region: 'Northeast' }
 ];
 
 async function main() {
@@ -45,13 +45,13 @@ async function main() {
         await client.connect();
 
         const database = client.db('dominican_republic');
-        const collection = database.collection('provincias');
+        const collection = database.collection('provinces');
 
         await collection.insertMany(provincias);
-        console.log('Datos insertados correctamente');
+        console.log('Data inserted correctly');
 
         const results = await collection.find({}).toArray();
-        console.log('Datos en la colección:');
+        console.log('Collections data:');
         console.log(results);
     } catch (error) {
         console.error('Error:', error);
